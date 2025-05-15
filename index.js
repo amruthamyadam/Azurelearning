205502ea96a7e98c516f448f6359040578ee2ec0
Sandhya Kaparthi <sandhya.kaparthi@frtinc.com>
2025-05-14 19:09:17 -0500

    change1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Azure Deploying!');
+  res.send('Hello from Azure Deploying I am adding more into GIT and push it to git hub!');
 });
 
 const port = process.env.PORT || 3000;
